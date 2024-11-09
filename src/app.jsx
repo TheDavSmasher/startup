8da966553458b0bfedaa897660d4824f6c5a4d1e
David Higueros <davhig22@byu.edu>
2024-11-09 13:50:05 -0700

    Added Routes

--- src/app.jsx
+++ src/app.jsx
@@ -6,34 +6,43 @@ import { Canvas } from './canvas/canvas';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
-function App() {
+export function App() {
     return (
         <BrowserRouter>
             <header className="container-fluid">
                 <nav className="navbar">
-                    <a className="nav-link" href="#"><h2>PixelShare&trade;</h2></a>
+                    <NavLink className="nav-link" href="#"><h2>PixelShare&trade;</h2></NavLink>
                     <menu className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" href="index.html">Login</a>
+                            <NavLink className="nav-link active" to="login">Login</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="canvas.html">Start Creating</a>
+                            <NavLink className="nav-link" to="canvas">Start Creating</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="share.html">Community Posts</a>
+                            <NavLink className="nav-link" to="share">Community Posts</NavLink>
                         </li>
                     </menu>
                 </nav>
             </header>
 
-            //Routes
+            <Routes>
+                <Route path="/" element={<Login />} exact />
+                <Route path="/share" element={<Share />} />
+                <Route path="/canvas" element={<Canvas />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
 
             <footer className="footer text-white-50">
                 <div className="footer-cont">
                     <span className="text-reset">David Higueros</span>
-                    <a className="text-reset" href="https://github.com/TheDavSmasher/startup">GitHub</a>
+                    <NavLink className="text-reset" href="https://github.com/TheDavSmasher/startup">GitHub</NavLink>
                 </div>
             </footer>
         </BrowserRouter>
     );
+}
+
+function NotFound() {
+    return <main>404: Return to sender. Address unknown.</main>
 }
