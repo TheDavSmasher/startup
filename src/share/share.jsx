1ca60aaf3d8c98b79a3c96bfee556b20dd4aae1c
David Higueros <davhig22@byu.edu>
2024-11-09 13:42:47 -0700

    Moved all Main HTML to jsx files

---
+++ src/share/share.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import './share.css';
+
+export function Share() {
+    return (
+        <main>
+            <div>
+                <h2>Recently Posted:</h2>
+                <div className="recent-posts">
+                    <div className="post">
+                        <div className="post-image-div">
+                            <img src="box.gif" className="post-image" />
+                        </div>
+                        <div className="credits">
+                            <span style="font-style: italic;">Breaker Box</span>
+                            <span>By: MaildropFolder</span>
+                        </div>
+                    </div>
+                    <div className="post">
+                        <div className="post-image-div">
+                            <img src="crow00.png" className="post-image" />
+                        </div>
+                        <div className="credits">
+                            <span style="font-style: italic;">Crow Sprite</span>
+                            <span>By: Maddie</span>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </main>
+    );
+}
