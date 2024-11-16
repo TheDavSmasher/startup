8874ff488840733d2999469832662a2d0218b5c4
David Higueros <davhig22@byu.edu>
2024-11-15 21:20:39 -0700

    Added Login and Logout Services

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -9,8 +9,16 @@ export function Authenticated(props) {
     const navigate = useNavigate();
 
     function logout() {
+        fetch(`/api/auth/logout`, {
+            method: 'delete',
+        })
+        .catch(() => {
+        // Logout failed. Assuming offline
+        })
+        .finally(() => {
         localStorage.removeItem('userName');
         props.onLogout();
+        });
     }
 
     return (
