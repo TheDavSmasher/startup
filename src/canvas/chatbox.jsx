db4b2fd1916e0bdc4cf8177a31d67ce22003f66e
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2024-12-06 18:03:10 -0700

    Fixed allowing pressing Enter to send message

--- src/canvas/chatbox.jsx
+++ src/canvas/chatbox.jsx
@@ -55,11 +55,17 @@ export function Chatbox(props) {
         <div className="chatbox">
             <div className="toast-container position-static">{createMessageArray()}</div>
             <div className="input-group send-message-form">
-                <input type="text" id="newMessage" className="form-control send-message-box" value={messageText} onChange={(e) => setMessageText(e.target.value)} />
+                <input type="text" id="newMessage" className="form-control send-message-box" value={messageText} onChange={(e) => setMessageText(e.target.value)} onKeyDown={(e) => {handleEnter(e, sendMessage)}} />
                 <div className="input-group-append">
-                    <button type="submit" className="btn btn-success" onClick={sendMessage} onKeyDown={(e) => {if (e.key === 'Enter') sendMessage();} } disabled={!messageText} >Send</button>
+                    <button type="submit" className="btn btn-success" onClick={sendMessage} disabled={!messageText} >Send</button>
                 </div>
             </div>
         </div>
     );
+}
+
+function handleEnter(event, message) {
+    if (event.key === 'Enter') {
+        message();
+    }
 }
